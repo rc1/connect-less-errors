3ad07db0da9996a3105a6b7d4a7296b35d3e390c
Ross Cairns <ross@electricglen.com>
2011-12-08 13:34:01 +0000

    Convert to spaces

--- less-errors.js
+++ less-errors.js
@@ -2,7 +2,7 @@
 var path = require('path');
 
 module.exports = function (err, req, res, next) {
-	if (path.extname(req.url) === '.css') {
+    if (path.extname(req.url) === '.css') {
         res.writeHead(200, {'Content-Type': 'text/css'});
         // wrap the error in css so chrome does not bail out
         res.end("/* " + JSON.stringify(err) + " */");

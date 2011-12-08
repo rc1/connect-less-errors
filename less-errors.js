0914b075ba06bef8d4ff1ad4af2c5b6ca01e7815
Ross Cairns <ross@electricglen.com>
2011-12-08 22:46:53 +0000

    ugly beautified

--- less-errors.js
+++ less-errors.js
@@ -3,9 +3,25 @@ var path = require('path');
 
 module.exports = function (err, req, res, next) {
     if (path.extname(req.url) === '.css') {
+        console.log(err);
+
+        var details =   "/* Less CSS: " +  err.name + "\n" +
+                        " * \n"+
+                        " * in     : " + path.basename(req.url, "css") + ".less \n" +
+                        " * line   : " + err.line + "\n" +
+                        " * column : " + err.column  + "\n" +
+                        " * errors :";
+
+        for (var index in err.extract) {
+            details +=  " " + err.extract[index]  + "\n *         ";
+        }
+
+        details +=      "\n" +
+                        " * Output : " +  JSON.stringify(err) + "\n" + 
+                        "*/";
+
         res.writeHead(200, {'Content-Type': 'text/css'});
-        // wrap the error in css so chrome does not bail out
-        res.end("/* " + JSON.stringify(err) + " */");
+        res.end(details);
     } else {
         next(err);
     }

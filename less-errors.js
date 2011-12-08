0ae1d6b80aa03db187c3fc7f8321a605b973b150
Ross Cairns <ross@electricglen.com>
2011-12-08 13:31:26 +0000

    added files

---
+++ less-errors.js
@@ -0,0 +1,12 @@
+
+var path = require('path');
+
+module.exports = function (err, req, res, next) {
+	if (path.extname(req.url) === '.css') {
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        // wrap the error in css so chrome does not bail out
+        res.end("/* " + JSON.stringify(err) + " */");
+    } else {
+        next(err);
+    }
+};
